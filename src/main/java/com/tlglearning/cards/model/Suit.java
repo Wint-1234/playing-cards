a17d0698411bb66685530cc2ea54a958d69f911f
Wint-1234 <winton.ebanks@yahoo.com>
2022-09-27 16:21:06 -0400

    Created a Main Class and implemented various methods.

--- src/main/java/com/tlglearning/cards/model/Suit.java
+++ src/main/java/com/tlglearning/cards/model/Suit.java
@@ -2,19 +2,25 @@ package com.tlglearning.cards.model;
 
 public enum Suit {
 
-  CLUBS('\u2663'),
-  DIAMONDS('\u2662'),
-  HEARTS('\u2661'),
-  SPADES('\u2660');
+  CLUBS('\u2663', Color.BLACK),
+  DIAMONDS('\u2662', Color.RED),
+  HEARTS('\u2661', Color.RED),
+  SPADES('\u2660', Color.BLACK);
 
   private final char symbol;
+  private final Color color;
 
-  private Suit(char symbol) {
+  Suit(char symbol, Color color) {
     this.symbol = symbol;
+    this.color = color;
   }
 
   public char symbol() {
     return symbol;
+  }
+
+  public Color color() {
+    return color;
   }
 
   @Override
@@ -22,4 +28,10 @@ public enum Suit {
     String name = name();
     return name.charAt(0) + name.substring(1).toLowerCase();
   }
+
+  public enum Color {
+    BLACK,
+    RED;
+  }
+
 }
